17ab9fdf2655e7e5928206c5a443b21c0a9e5b73
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-09-09 22:26:30 +0200

    feat: remove TCP connector declaration from app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { LocalAuthModule } from './local-auth/local-auth.module';
 import { DbModule } from './db/db.module';
 import { GoogleAuthModule } from './google-auth/google-auth.module';
@@ -8,14 +7,6 @@ import { FacebookAuthModule } from './facebook-auth/facebook-auth.module';
 
 @Module({
   imports: [
-    ClientsModule.register([{
-      name: 'ROOT_ENGINE',
-      transport: Transport.TCP,
-      options: {
-        host: '127.0.0.1',
-        port: 64320
-      }
-    }]),
     ConfigModule.forRoot(),
     DbModule,
     LocalAuthModule,
