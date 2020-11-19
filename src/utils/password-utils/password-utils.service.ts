299ff0f2174eef739e5263a86f1f8d3316c5cd81
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-11-19 19:49:02 +0100

    doc-#37: add documentation to password utils service methods

--- src/utils/password-utils/password-utils.service.ts
+++ src/utils/password-utils/password-utils.service.ts
@@ -5,18 +5,44 @@ import * as bcrypt from 'bcrypt';
 @Injectable()
 export class PasswordUtilsService {
   constructor(private readonly configService: ConfigService) {}
-
-  async hashPassword(password: string, salt: string): Promise<string> {
+  /**
+   * Method that hashes the password with salt provided as parameter.
+   * Uses `bcrypt.hash()` method with password and salt as parameters. 
+   * Also, adds `HASH_PEPPER` environment variable to password.
+   * @param {string} [password]
+   * @param {string} [salt]
+   * @return {*}  {String}
+   * @memberof PasswordUtilsService
+   */
+  async hashPassword(
+    password: string,
+    salt: string
+  ): Promise<string> {
     return await bcrypt.hash(
       password + this.configService.get<string>('HASH_PEPPER'),
       salt,
     );
   }
 
+  /**
+   * Method that generates `salt` that can be added to the password when signing up.
+   * Uses `bcrypt.getSalt()` method and returns a string salt.
+   * @return {*}  {String}
+   * @memberof PasswordUtilsService
+   */
   async generateSalt(): Promise<string> {
     return await bcrypt.genSalt();
   }
 
+  /**
+   * Method that checks whether provided password matches user password from database.
+   * Uses `bcrypt.compare()` method that returns true when two passwords match.
+   * Also, adds `HASH_PEPPER` environment variable to password.
+   * @param {string} [providedPassword]
+   * @param {string} [userPassword]
+   * @return {Boolean}  {Boolean}
+   * @memberof PasswordUtilsService
+   */
   async validatePassword(
     providedPassword: string,
     userPassword: string,
