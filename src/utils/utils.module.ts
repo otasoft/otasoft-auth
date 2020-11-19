bd68c454d3c014885f54d760ef822abca00439f6
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-11-19 19:38:03 +0100

    refactor-#37: add exception and error handling methods from catalog microservice

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { ErrorValidationService } from './error-validation';
+import { RpcExceptionService } from './exception-handling';
 import { PasswordUtilsService } from './password-utils';
 
 @Module({
   imports: [ConfigModule],
-  providers: [PasswordUtilsService],
-  exports: [PasswordUtilsService],
+  providers: [PasswordUtilsService, RpcExceptionService, ErrorValidationService],
+  exports: [PasswordUtilsService, RpcExceptionService, ErrorValidationService],
 })
 export class UtilsModule {}
