60f7ee1eec2c814c48d0ecbd869e6e4e1660f59d
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-08 14:05:44 +0100

    feat-#62: fix authentication tests

--- src/auth/authentication/services/authentication.service.spec.ts
+++ src/auth/authentication/services/authentication.service.spec.ts
@@ -1,12 +1,34 @@
+import { ConfigService } from '@nestjs/config';
+import { CqrsModule } from '@nestjs/cqrs';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { JwtTokenService } from '../../passport-jwt/services';
+import { UserService } from '../../user/services/user.service';
+import { mockedConfigService, mockedJwtService } from '../../../utils/mocks';
 import { AuthenticationService } from './authentication.service';
+import { RpcExceptionService } from '../../../utils/exception-handling';
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthenticationService],
+      imports: [CqrsModule],
+      providers: [
+        AuthenticationService,
+        UserService,
+        JwtTokenService,
+        RpcExceptionService,
+        {
+          provide: JwtService,
+          useValue: mockedJwtService,
+        },
+        {
+          provide: ConfigService,
+          useValue: mockedConfigService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthenticationService>(AuthenticationService);
