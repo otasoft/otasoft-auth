cd570811411bdbb465010fc71ecaae83f12eecfe
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-08 13:28:30 +0100

    feat-#62: fix user related tests

--- src/auth/user/controllers/user.controller.spec.ts
+++ src/auth/user/controllers/user.controller.spec.ts
@@ -1,12 +1,18 @@
+import { CqrsModule } from '@nestjs/cqrs';
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { UserService } from '../services/user.service';
 import { UserController } from './user.controller';
+import { RpcExceptionService } from '../../../utils/exception-handling';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CqrsModule],
       controllers: [UserController],
+      providers: [UserService, RpcExceptionService]
     }).compile();
 
     controller = module.get<UserController>(UserController);
