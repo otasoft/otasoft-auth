29b9542d75b5925d292233d2e56bb3225d0cd5ac
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-26 20:08:15 +0100

    feat-#68: add user write repo to authorization

--- src/auth/authorization/authorization.module.ts
+++ src/auth/authorization/authorization.module.ts
@@ -3,14 +3,14 @@ import { ConfigService } from '@nestjs/config';
 import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { UserRepository } from '../../db/repositories';
+import { UserRepository, UserWriteRepository } from '../../db/repositories';
 import { AuthorizationController } from './controllers/authorization.controller';
 import { AuthorizationService } from './services/authorization.service';
 import { CommandHandlers } from './commands/handlers';
 import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository]), CqrsModule, UserModule],
+  imports: [TypeOrmModule.forFeature([UserRepository, UserWriteRepository]), CqrsModule, UserModule],
   controllers: [AuthorizationController],
   providers: [AuthorizationService, ConfigService, ...CommandHandlers],
 })
