59e672904f91d0d007f83f6f7e83f1e721f80abe
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-27 10:32:29 +0100

    feat-#67: create Cookie Service

---
+++ src/auth/authentication/services/cookie.service.spec.ts
@@ -0,0 +1,33 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { JwtTokenService } from '../../passport-jwt/services';
+import { mockedConfigService, mockedJwtService } from '../../../utils/mocks';
+import { CookieService } from './cookie.service';
+
+describe('CookieService', () => {
+  let service: CookieService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CookieService,
+        {
+          provide: JwtService,
+          useValue: mockedJwtService,
+        },
+        {
+          provide: ConfigService,
+          useValue: mockedConfigService,
+        },
+      ],
+    }).compile();
+
+    service = module.get<CookieService>(CookieService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
---
+++ src/auth/authentication/services/index.ts
@@ -0,0 +1,2 @@
+export * from './authentication.service';
+export * from './cookie.service';
